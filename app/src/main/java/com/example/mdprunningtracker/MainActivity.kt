a866c96a4c47240e60a69e980d5769bd41158427
James Scully <james.will.scully@gmail.com>
2019-12-27 19:00:36 +0000

    Added Design notes, created main activity accoridng to this.

--- app/src/main/java/com/example/mdprunningtracker/MainActivity.kt
+++ app/src/main/java/com/example/mdprunningtracker/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // hide our action bar; it isn't needed here (yet)
+        supportActionBar?.hide()
+
+
     }
+
+
+
+
 }
